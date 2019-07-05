2e980da19f2fd0b0d935a2af7c3a644a6f00a303
kirubarajm <kirubarajmathavan@gmail.com>
2019-07-05 19:32:29 +0530

    Region list completed Region details page completed Region card view more completed

---
+++ app/src/main/java/com/tovo/eat/ui/home/region/viewmore/RegionsListAdapter.java
@@ -0,0 +1,159 @@
+package com.tovo.eat.ui.home.region.viewmore;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tovo.eat.data.DataManager;
+import com.tovo.eat.databinding.ListItemEmptyBinding;
+import com.tovo.eat.databinding.ListItemKitchenDishesBinding;
+import com.tovo.eat.databinding.ListItemRegionListBinding;
+import com.tovo.eat.ui.base.BaseViewHolder;
+import com.tovo.eat.ui.home.homemenu.RegionCardMoreItemViewModel;
+import com.tovo.eat.ui.home.homemenu.kitchen.EmptyItemViewModel;
+import com.tovo.eat.ui.home.kitchendish.KitchenDishItemViewModel;
+import com.tovo.eat.ui.home.kitchendish.KitchenDishResponse;
+import com.tovo.eat.ui.home.region.RegionsResponse;
+
+import java.util.List;
+
+public class ReegionsListAdapter extends RecyclerView.Adapter<BaseViewHolder> {
+
+    private static final int VIEW_TYPE_NORMAL = 1;
+    private static final int VIEW_TYPE_EMPTY = 0;
+    List<KitchenDishResponse.Result> response;
+    private List<RegionsResponse.Result> item_list;
+    private LiveProductsAdapterListener mLiveProductsAdapterListener;
+    private DataManager dataManager;
+
+
+    public ReegionsListAdapter(List<RegionsResponse.Result> item_list) {
+        this.item_list = item_list;
+    }
+
+
+    public ReegionsListAdapter(List<RegionsResponse.Result> item_list, DataManager dataManager) {
+        this.item_list = item_list;
+        this.dataManager = dataManager;
+    }
+
+
+    @Override
+    public BaseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        switch (i) {
+            case VIEW_TYPE_NORMAL:
+                ListItemRegionListBinding blogViewBinding = ListItemRegionListBinding.inflate(LayoutInflater.from(parent.getContext()),
+                        parent, false);
+                return new LiveProductsViewHolder(blogViewBinding);
+            case VIEW_TYPE_EMPTY:
+            default:
+                ListItemEmptyBinding blogViewBinding1 = ListItemEmptyBinding.inflate(LayoutInflater.from(parent.getContext()),
+                        parent, false);
+                return new EmptyViewHolder(blogViewBinding1);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaseViewHolder baseViewHolder, int i) {
+        baseViewHolder.onBind(i);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (item_list != null && item_list.size() > 0) {
+            return item_list.size();
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (item_list != null && !item_list.isEmpty()) {
+            return VIEW_TYPE_NORMAL;
+        } else {
+            return VIEW_TYPE_EMPTY;
+        }
+    }
+
+    public void clearItems() {
+        item_list.clear();
+    }
+
+    public void addItems(List<RegionsResponse.Result> blogList, List<KitchenDishResponse.Result> response) {
+        item_list.addAll(blogList);
+
+        this.response = response;
+
+        notifyDataSetChanged();
+    }
+
+    public void setListener(LiveProductsAdapterListener listener) {
+        this.mLiveProductsAdapterListener = listener;
+    }
+
+    public interface LiveProductsAdapterListener {
+
+        void onItemClickData(Integer regionId);
+
+
+    }
+
+
+    public class EmptyViewHolder extends BaseViewHolder {
+
+        private final ListItemEmptyBinding mBinding;
+
+
+        EmptyItemViewModel emptyItemViewModel;
+
+        public EmptyViewHolder(ListItemEmptyBinding binding) {
+            super(binding.getRoot());
+            this.mBinding = binding;
+        }
+
+        @Override
+        public void onBind(int position) {
+            emptyItemViewModel = new EmptyItemViewModel("No results found for your selection");
+            mBinding.setEmptyItemViewModel(emptyItemViewModel);
+        }
+
+    }
+
+    public class LiveProductsViewHolder extends BaseViewHolder implements RegionsListItemViewModel.RegionItemViewModelListener {
+        ListItemRegionListBinding mListItemLiveProductsBinding;
+        RegionsListItemViewModel mLiveProductsItemViewModel;
+
+        public LiveProductsViewHolder(ListItemRegionListBinding binding) {
+            super(binding.getRoot());
+            this.mListItemLiveProductsBinding = binding;
+
+        }
+
+        @Override
+        public void onBind(int position) {
+            if (item_list.isEmpty()) return;
+            final RegionsResponse.Result blog = item_list.get(position);
+
+            mLiveProductsItemViewModel = new RegionsListItemViewModel(this, blog);
+            mListItemLiveProductsBinding.setRegionItemViewModel(mLiveProductsItemViewModel);
+
+            // Immediate Binding
+            // When a variable or observable changes, the binding will be scheduled to change before
+            // the next frame. There are times, however, when binding must be executed immediately.
+            // To force execution, use the executePendingBindings() method.
+            mListItemLiveProductsBinding.executePendingBindings();
+        }
+
+
+        @Override
+        public void onItemClick(Integer id) {
+            mLiveProductsAdapterListener.onItemClickData(id);
+        }
+    }
+
+}
